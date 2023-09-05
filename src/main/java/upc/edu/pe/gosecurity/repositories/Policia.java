25d04968d4d034ad525177e713207d73d3115474
Kacchan368 <angelochipulina@gmail.com>
2023-09-04 23:58:51 -0500

    Repository comi poli y ubi

---
+++ src/main/java/upc/edu/pe/gosecurity/repositories/Policia.java
@@ -0,0 +1,6 @@
+package upc.edu.pe.gosecurity.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Policia extends JpaRepository<Policia,Integer> {
+}
---
+++ src/main/java/upc/edu/pe/gosecurity/repositories/UbicacionRepository.java
@@ -0,0 +1,7 @@
+package upc.edu.pe.gosecurity.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import upc.edu.pe.gosecurity.entities.Ubicacion;
+
+public interface UbicacionRepository extends JpaRepository<Ubicacion, Integer> {
+}
