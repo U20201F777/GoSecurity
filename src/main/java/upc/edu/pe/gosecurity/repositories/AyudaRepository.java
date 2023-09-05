507b0beeda012cd1bdf1c8b0ba16f76f680f437d
Kacchan368 <angelochipulina@gmail.com>
2023-09-05 08:48:06 -0500

    Revert "Merge remote-tracking branch 'origin/Adrian' into Angelo"
    This reverts commit 834a4cc71dae6c9c1d322b4430aab106efc8a7e3, reversing
    changes made to 6401d4210be8c766bb1a4a1dc043b75f9f59ce6d.

--- src/main/java/upc/edu/pe/gosecurity/repositories/AyudaRepository.java
+++ src/main/java/upc/edu/pe/gosecurity/repositories/AyudaRepository.java
@@ -2,8 +2,8 @@ package upc.edu.pe.gosecurity.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import upc.edu.pe.gosecurity.entities.PertenenciasTipo;
+import upc.edu.pe.gosecurity.entities.Ayuda;
 
 @Repository
-public interface PTipoRepository extends JpaRepository<PertenenciasTipo,Integer> {
+public interface AyudaRepository extends JpaRepository<Ayuda, Integer> {
 }
--- src/main/java/upc/edu/pe/gosecurity/repositories/NotificacionRepository.java
+++ src/main/java/upc/edu/pe/gosecurity/repositories/NotificacionRepository.java
@@ -2,8 +2,7 @@ package upc.edu.pe.gosecurity.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import upc.edu.pe.gosecurity.entities.PertenenciasModelo;
-
+import upc.edu.pe.gosecurity.entities.Notificacion;
 @Repository
-public interface PModeloRepository extends JpaRepository <PertenenciasModelo,Integer> {
+public interface NotificacionRepository extends JpaRepository<Notificacion, Integer> {
 }
