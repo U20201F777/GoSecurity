2d61956cb297a4edd2dc8006f4824965b3b2f37a
Kacchan368 <angelochipulina@gmail.com>
2023-09-10 20:37:54 -0500

    Revert "Merge remote-tracking branch 'origin/Angelo'"
    This reverts commit b77081b9ab9d566aa2d8cd0e60b2b2c73b9cd1e4, reversing
    changes made to 359963567ed21166be7b1c98cf1d2f8389419369.

--- src/main/java/upc/edu/pe/gosecurity/repositories/UbicacionCRepository.java
+++
@@ -1,8 +0,0 @@
-package upc.edu.pe.gosecurity.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import upc.edu.pe.gosecurity.entities.UbicacionC;
-
-public interface UbicacionCRepository extends JpaRepository<UbicacionC, Integer> {
-
-}
--- src/main/java/upc/edu/pe/gosecurity/repositories/UbicacionPRepository.java
+++
@@ -1,7 +0,0 @@
-package upc.edu.pe.gosecurity.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import upc.edu.pe.gosecurity.entities.UbicacionP;
-
-public interface UbicacionPRepository extends JpaRepository<UbicacionP, Integer> {
-}
