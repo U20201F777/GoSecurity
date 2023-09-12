0c5ffbbde8f9ab84390be438ba29bb5a3f06c769
Kacchan368 <angelochipulina@gmail.com>
2023-09-11 23:28:47 -0500

    entities

--- src/main/java/upc/edu/pe/gosecurity/serviceimplements/IUbicacionPImplements.java
+++ src/main/java/upc/edu/pe/gosecurity/serviceimplements/IUbicacionPImplements.java
@@ -2,7 +2,6 @@ package upc.edu.pe.gosecurity.serviceimplements;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import upc.edu.pe.gosecurity.entities.UbicacionP;
 import upc.edu.pe.gosecurity.repositories.UbicacionPRepository;
 import upc.edu.pe.gosecurity.serviceinterfaces.IUbicacionPService;
 
